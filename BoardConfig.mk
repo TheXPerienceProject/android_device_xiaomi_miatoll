f11a05d638d338da7910da68ecfd899d50da2fa6
klozz <klozz@TheXPerienceProject.org>
2020-09-10 10:04:56 +0000

    Atoll:  remove sepolicy for Red Velvet cake bring
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,10 +199,10 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2020-07-01
 
 # Sepolicy
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 #
 BOARD_VNDK_VERSION := current
