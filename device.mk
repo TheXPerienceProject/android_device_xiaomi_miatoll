0f333fdd776553c88acf239413d63f71ce6457d3
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-16 11:38:18 +0300

    Atoll: copy libhidlcache from vndk 29
    * needed by vendor.xiaomi.hardware.misys@3.0-service
    
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- device.mk
+++ device.mk
@@ -414,6 +414,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator@1.2-service
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libhidlcache.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlcache.so
+
 # Vulkan
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
