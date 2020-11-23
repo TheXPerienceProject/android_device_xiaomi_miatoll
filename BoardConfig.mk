7a1639337f4cd3c382e7dac3d7641a3eadda124a
klozz <klozz@TheXPerienceProject.org>
2020-11-23 04:40:36 +0100

    Atoll: Enable the sepolicy I will cleaning it up when needed
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,9 +202,9 @@ VENDOR_SECURITY_PATCH := 2020-07-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-#BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-#BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 #
 BOARD_VNDK_VERSION := current
