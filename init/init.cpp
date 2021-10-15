5b073d6f27f23be70fb0665c80a9b88162f2e521
klozz <klozz@thexperienceproject.org>
2021-10-15 08:35:43 +0200

    miatoll: set FP from our device taken from stock
    Signed-off-by: klozz <klozz@thexperienceproject.org>

--- init/init.cpp
+++ init/init.cpp
@@ -70,29 +70,29 @@ void load_device_properties() {
     if (hwname == "curtana") {
         if (region == "Global_TWO") {
             set_device_props(
-                "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys",
-                "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys",
+                "Redmi/curtana_global/curtana:11/RKQ1.200826.002/V12.5.1.0.RJWMIXM:user/release-keys",
+                "curtana_global-user 11 RKQ1.200826.002 V12.5.1.0.RJWMIXM release-keys",
                 "Redmi", "curtana", "Redmi Note 9S");
         } else if (region == "India") {
             set_device_props(
-                "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys",
-                "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys",
+                "Redmi/curtana/curtana:11/RKQ1.200826.002/V12.5.1.0.RJWINXM:user/release-keys",
+                "curtana_in-user 11 RKQ1.200826.002 V12.5.1.0.RJWINXM release-keys",
                 "Redmi", "curtana", "Redmi Note 9 Pro");
         }
     } else if (hwname == "excalibur") {
         set_device_props(
-                "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys",
-                "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys",
+                "Redmi/excalibur_in/excalibur:11/RKQ1.200826.002/V12.5.2.0.RJXINXM:user/release-keys",
+                "excalibur_in-user 11 RKQ1.200826.002 V12.5.2.0.RJXINXM release-keys",
                 "Redmi", "excalibur", "Redmi Note 9 Pro Max");
     } else if (hwname == "gram") {
         set_device_props(
-                "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys",
-                "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys",
+                "POCO/gram_in/gram:11/RKQ1.200826.002/V12.0.1.0.RJPINXM:user/release-keys",
+                "gram_in-user 11 RKQ1.200826.002 V12.0.1.0.RJPINXM release-keys",
                 "POCO", "gram", "POCO M2 Pro");
     } else if (hwname == "joyeuse") {
         set_device_props(
-                "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys",
-                "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys",
+                "Redmi/joyeuse_eea/joyeuse:11/RKQ1.200826.002/V12.5.1.0.RJZEUXM:user/release-keys",
+                "joyeuse_eea-user 11 RKQ1.200826.002 V12.5.1.0.RJZEUXM release-keys",
                 "Redmi", "joyeuse", "Redmi Note 9 Pro");
     }
 }
